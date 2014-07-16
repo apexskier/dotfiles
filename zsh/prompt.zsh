375957384c30fcbbb4c096ccec09f85245f314b4
Cameron Little <cameron@camlittle.com>
2014-07-16 09:59:59 -0700

    Working on custom zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -12,7 +12,6 @@ fi
 git_branch() {
   echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
-
 git_dirty() {
   if $(! $git status -s &> /dev/null)
   then
@@ -20,17 +19,18 @@ git_dirty() {
   else
     if [[ $($git status --porcelain) == "" ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+        echo -n "(±%{$fg[white]%}$(git_prompt_info)%{$reset_color%}"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+        echo -n "(±%{$fg_bold[white]%}$(git_prompt_info)%{$reset_color%}"
     fi
   fi
+  echo "$(unpushed)) "
 }
 
 git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
+  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+  # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+  echo "${ref#refs/heads/}"
 }
 
 unpushed () {
@@ -40,9 +40,9 @@ unpushed () {
 need_push () {
   if [[ $(unpushed) == "" ]]
   then
-    echo " "
+    echo ""
   else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    echo " %{$fg[grey]%}needs push%{$reset_color%}"
   fi
 }
 
@@ -67,11 +67,44 @@ rb_prompt() {
   fi
 }
 
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+virtualenv() {
+  if [[ -n "$VIRTUAL_ENV" ]]
+  then
+    echo="(⊙${VIRTUAL_ENV#$WORKON_HOME/}) "
+  fi
+}
+NEWLINE=$'\n'
+
+cmd_info() {
+  if [[ "$?" != "0" ]]
+  then
+    echo "%{$fg_bold[red]%}%?↩%{$reset_color%} "
+  fi
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+username="%{$fg[cyan]%}%n%{$reset_color%}"
+hostname="%{$fg[green]%}%m%{$reset_color%}"
+rw() {
+  if [[ -w "$PWD" ]]
+  then
+    echo "%{$fg[green]%}:%{$reset_color%}"
+  else
+    echo "%{$fg[red]%}:%{$reset_color%}"
+  fi
+}
+directory() {
+  echo "%{$fg[yellow]%}%~%{$reset_color%}"
+}
+
+mark="@"
+# If we are connected with a X11 support
+if [[ -n "$DISPLAY" ]] ; then
+    mark="%{$fg_bold[white]%}$mark$reset_color"
+else
+    mark="%{$fg[white]%}$mark$reset_color"
+fi
+
+export PROMPT=$'$(cmd_info)${username}${mark}$(rb_prompt)${hostname}$(rw)$(directory) $(virtualenv)$(git_dirty)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
