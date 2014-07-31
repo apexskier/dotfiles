e5050a6142af40bee51f8cbe0803458c2331fe3f
Cameron Little <cameron@camlittle.com>
2014-07-30 19:08:15 -0700

    tested out osx install file

--- osx/install.sh
+++ osx/install.sh
@@ -4,5 +4,8 @@ then
     brew bundle `git rev-parse --show-cdup`osx/Brewfile
     echo "Installing apps"
     brew bundle `git rev-parse --show-cdup`osx/Caskfile
+    for f in $HOME/Applications/*; do sudo ln -sf "$f" /Applications/; done
+
+    echo "you may want to install xQuartz -> http://xquartz.macosforge.org/landing/"
 fi
 
