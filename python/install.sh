26399158cce9156b36b3c89143df9feb297dd883
Cameron Little <cameron@camlittle.com>
2014-10-15 21:49:46 -0700

    improvements to install.sh's

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
-echo "  Installing pip/virtualenv..."
-easy_install pip > /dev/null
-pip install virtualenv > /dev/null
-pip install virtualenvwrapper > /dev/null
+if ! hash mkvirtualenv
+then
+    echo "  Installing pip/virtualenv..."
+    easy_install -U pip > /dev/null
+    pip install virtualenv > /dev/null
+    pip install virtualenvwrapper > /dev/null
+fi
