1e4866636f06467f20867e0ffa92cf38708b5f5c
Cameron Little <cameron.little@wwu.edu>
2014-11-18 13:00:56 -0800

    Fixing up installing "config" files.

--- vim/install.sh
+++ vim/install.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > /dev/null | sh > /dev/null
+(curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh) > /dev/null
