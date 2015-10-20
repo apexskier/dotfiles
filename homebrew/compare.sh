3f6657ec01d87afab60d73a29c11aaef6e0b0602
Cameron Little <cameron@camlittle.com>
2015-10-19 17:16:49 -0700

    Update brewfile. Added comparison script.

---
+++ homebrew/compare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Use this to compare what's currently installed with what's listed here:
+
+A=/tmp/tempA.txt
+B=/tmp/tempB.txt
+echo "Installed" > $A
+echo "---" >> $A
+brew leaves | sort >> $A
+echo "In Config" > $B
+echo "---" >> $B
+cat ~/.dotfiles/homebrew/Brewfile | grep '^brew install' | cut -d' ' -f3 | sort >> $B
+vimdiff /tmp/tempA.txt /tmp/tempB.txt
