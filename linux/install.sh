5033700171cc13e363954714e62c2b6bfb4f4a29
Cameron Little <cameron@camlittle.com>
2016-01-30 19:32:38 +0000

    Updated linux install.

--- linux/install.sh
+++ linux/install.sh
@@ -5,5 +5,5 @@ then
     echo "  Installing linux utilities"
     sudo apt-get update
     sudo apt-get upgrade
-    sudo apt-get install wget curl tmux vim git awesome easy_install
+    sudo apt-get install wget curl tmux vim git
 fi
