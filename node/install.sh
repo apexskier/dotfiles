ff060a740c746245cb85a8437db87dc38577cc54
Cameron Little <cameron@camlittle.com>
2016-06-23 17:05:07 -0700

    Added better nvm/node/npm setup

--- node/install.sh
+++ node/install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# if you don't have sudo --> https://gist.github.com/isaacs/579814
-
-if [ "$(uname -s)" == "Darwin" ] # use homebrew on mac
-then
-    echo '  installing node'
-    curl https://raw.githubusercontent.com/creationix/nvm/v0.20.0/install.sh >/dev/null 2>/dev/null | bash >/dev/null 2>/dev/null
-    test -O ~/.nvm || sudo chown -R $USER ~/.nvm
-    source ~/.nvm/nvm.sh >/dev/null
-    nvm install 0.10 >/dev/null # replace with stable
-    nvm alias default 0.10 >/dev/null # replace with stable
-else
-    # TODO:
-    echo "  You should figure out how to install node and npm."
+echo '  installing node'
+curl https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh >/dev/null 2>/dev/null | bash >/dev/null 2>/dev/null
+export NVM_DIR="$HOME/.nvm"
+if [ -d "$NVM_DIR" ]; then
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 fi
+nvm install node >/dev/null
+nvm alias default node >/dev/null
