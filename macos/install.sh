92fb47f57750db2c6d8c9c750a091710a57fdd09
Cameron Little <cameron@remitly.com>
2019-08-19 15:43:48 -0700

    More cleanup, including removing tools I don't use

--- macos/install.sh
+++ macos/install.sh
@@ -1,9 +1,6 @@
-# The Brewfile handles Homebrew-based app and library installs, but there may
-# still be updates and installables in the Mac App Store. There's a nifty
-# command line interface to it that we can use to just install everything, so
-# yeah, let's do that.
+# Homebrew-based installs are separate, but there may still be updates and
+# installables in the Mac App Store. There's a nifty command line interface to
+# it that we can use to just install everything, so yeah, let's do that.
 
 echo "› sudo softwareupdate -i -a"
 sudo softwareupdate -i -a
-
-curl -o ~/.yarn-completion https://raw.githubusercontent.com/dsifford/yarn-completion/master/yarn-completion.bash
---
+++ node/install.sh
@@ -0,0 +1,1 @@
+curl -o ~/.yarn-completion https://raw.githubusercontent.com/dsifford/yarn-completion/master/yarn-completion.bash
