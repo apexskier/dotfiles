f56908a50f205e0ce8c2d2e12a27d0b336867bbf
Cameron Little <cameron@remitly.com>
2019-10-24 12:03:17 +0200

    Add standard ~/Dev location and don't spotlight index it

--- macos/config.sh
+++ macos/config.sh
@@ -11,6 +11,9 @@ then
     #
     # Run ./set-defaults.sh and you'll be good to go.
 
+    mkdir -p ~/Dev
+    touch ~/Dev/.metadata_never_index
+
     # Disable press-and-hold for keys in favor of key repeat.
     defaults write -g ApplePressAndHoldEnabled -bool false
 
