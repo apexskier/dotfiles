72df7327231b5b257346797250edb674999dc0a9
Cameron Little <cameron@remitly.com>
2019-12-18 15:41:37 +0100

    remove a bunch of stuff I haven't used in a while - outdated is worse than non-existant

--- node/install.sh
+++
@@ -1,1 +0,0 @@
-curl -o ~/.yarn-completion https://raw.githubusercontent.com/dsifford/yarn-completion/master/yarn-completion.bash
--- python/install.sh
+++
@@ -1,7 +0,0 @@
-if ! command -v mkvirtualenv >/dev/null 2>&1
-then
-    echo "  Installing pip/virtualenv..."
-    easy_install -U pip > /dev/null
-    pip install virtualenv > /dev/null
-    pip install virtualenvwrapper > /dev/null
-fi
