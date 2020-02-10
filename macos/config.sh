e724e3c6a78342d91fc7643403a7b09f705db953
Cameron Little <cameron@remitly.com>
2020-02-10 10:38:28 +0100

    finder prefs changes

--- macos/config.sh
+++ macos/config.sh
@@ -20,9 +20,6 @@ then
     # Use AirDrop over every interface. srsly this should be a default.
     defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-    # Always open everything in Finder's column view.
-    defaults write com.apple.Finder FXPreferredViewStyle clmv
-
     # Show the ~/Library folder.
     chflags nohidden ~/Library
 
@@ -31,8 +28,8 @@ then
     defaults write NSGlobalDomain InitialKeyRepeat -int 20
 
     # Set the Finder prefs for showing a few different volumes on the Desktop.
-    defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-    defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+    defaults write com.apple.Finder ShowExternalHardDrivesOnDesktop -bool true
+    defaults write com.apple.Finder ShowRemovableMediaOnDesktop -bool true
 
     # Set up Safari for development.
     defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
@@ -77,27 +74,27 @@ then
     defaults write com.apple.screencapture type -string "png"
 
     # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-    defaults write com.apple.finder QuitMenuItem -bool true
+    defaults write com.apple.Finder QuitMenuItem -bool true
 
     # Set the default location for new Finder windows
     # For other paths, use `PfLo` and `file:///full/path/here/`
-    defaults write com.apple.finder NewWindowTarget -string "PfLo"
-    # defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Dev/"
+    defaults write com.apple.Finder NewWindowTarget -string "PfLo"
+    # defaults write com.apple.Finder NewWindowTargetPath -string "file://${HOME}/Dev/"
 
     # Finder: show path bar
-    defaults write com.apple.finder ShowPathbar -bool true
+    defaults write com.apple.Finder ShowPathbar -bool true
 
     # Finder: show status bar
-    defaults write com.apple.finder ShowStatusBar -bool true
+    defaults write com.apple.Finder ShowStatusBar -bool true
 
     # Finder: allow text selection in Quick Look
-    defaults write com.apple.finder QLEnableTextSelection -bool true
+    defaults write com.apple.Finder QLEnableTextSelection -bool true
 
     # When performing a search, search the current folder by default
-    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    defaults write com.apple.Finder FXDefaultSearchScope -string "SCcf"
 
     # Disable the warning when changing a file extension
-    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+    defaults write com.apple.Finder FXEnableExtensionChangeWarning -bool false
 
     # Avoid creating .DS_Store files on network volumes
     defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -105,11 +102,11 @@ then
     # Automatically open a new Finder window when a volume is mounted
     defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
     defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-    defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+    defaults write com.apple.Finder OpenWindowForNewRemovableDisk -bool true
 
     # Use column view in all Finder windows by default
     # Four-letter codes for the other view modes: `Nlsv`, `icnv`, `clmv`, `Flwv`
-    defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+    defaults write com.apple.Finder FXPreferredViewStyle "clmv"
 
     # Enable AirDrop over Ethernet and on unsupported Macs running Lion
     defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
@@ -123,7 +120,7 @@ then
 
     # Expand the following File Info panes:
     # “General”, “Open with”, and “Sharing & Permissions”
-    defaults write com.apple.finder FXInfoPanesExpanded -dict \
+    defaults write com.apple.Finder FXInfoPanesExpanded -dict \
         General -bool true \
         OpenWith -bool true \
         Privileges -bool true
