af167d64bbf5ddce391ddbdaa92ac9dd4e234f83
Cameron Little <cameron@camlittle.com>
2020-03-21 17:52:48 +0100

    remove docker login

--- docker/install.sh
+++ docker/install.sh
@@ -7,8 +7,7 @@ then
     exit 0
 fi
 
-# this doesn't install docker, but pulls our bash docker image in
+# this doesn't install docker, but pulls my docker toolbox image in
 
-cat ~/.GITHUB_TOKEN | docker login docker.pkg.github.com -u apexskier --password-stdin
 docker pull docker.pkg.github.com/apexskier/dotfiles/toolbox:latest
 docker tag docker.pkg.github.com/apexskier/dotfiles/toolbox:latest toolbox
