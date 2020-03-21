a4ef60cf81bcf11a45a05d60954a4498e96b4eb8
Cameron Little <cameron@camlittle.com>
2020-03-21 16:23:56 +0100

    cleanup

--- linux/install.sh
+++ linux/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if command -v apt-get >/dev/null 2>&1
+if ! command -v apt-get >/dev/null 2>&1
 then
-    echo "  Installing linux utilities"
-    sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get install wget curl tmux vim git
+    exit 0
 fi
+
+echo "  Installing linux utilities"
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install wget curl tmux vim git
