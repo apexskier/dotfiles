ea3a129f2a9c3c8e6896a740a9ae9196235cd6c2
Cameron Little <cameron@camlittle.com>
2020-03-21 16:51:05 +0100

    Add install script for docker toolbox image

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if ! command -v docker >/dev/null 2>&1
 then
     exit 0
@@ -7,3 +9,6 @@ fi
 
 # this doesn't install docker, but pulls our bash docker image in
 
+cat ~/.GITHUB_TOKEN | docker login docker.pkg.github.com -u apexskier --password-stdin
+docker pull docker.pkg.github.com/apexskier/dotfiles/toolbox:latest
+docker tag docker.pkg.github.com/apexskier/dotfiles/toolbox:latest toolbox
