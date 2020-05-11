3a920cf27c176b637d4504435a4433da6df89d45
Cameron Little <cameron@remitly.com>
2020-05-11 14:09:27 +0200

    faster and non-permanent chnode

--- nodejs/nodejs.bash
+++ nodejs/nodejs.bash
@@ -5,11 +5,29 @@ export REACT_EDITOR_CMD=code
 export REACT_EDITOR_PATTERN="-r -g {filename}:{line}:{column}"
 
 function chnode() {
-    local version
+    if [ "$(uname -s)" != "Darwin" ]; then
+        echo "macos only"
+        return 1
+    fi
+
+    PATH_PREFIX="$(brew --prefix)/Cellar/node"
+
+    # clear prior path
+    # https://stackoverflow.com/a/370192/2178159
+    export PATH=$(echo "$PATH" | awk -v RS=: -v ORS=: "/${PATH_PREFIX//\//\\/}/ {next} {print}" | sed 's/:*$//')
+
     if [ -n "$1" ]
     then
-        version="@$1"
+        local NODE_PATH=$(eval echo "$PATH_PREFIX*/$1*/bin/")
+        if [ -d "$NODE_PATH" ]
+        then
+            echo "using node $(echo "$NODE_PATH" | rev | cut -d'/' -f3 | rev)"
+            export PATH="$NODE_PATH:$PATH"
+        else
+            echo "no node installation found for '$1'"
+            return 1
+        fi
+    else
+        echo "using system installation"
     fi
-    brew unlink $(brew list | grep node)
-    brew link --overwrite --force "node$version"
 }
