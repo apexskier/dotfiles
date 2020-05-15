936dccd02cae6d7dede684cc81e71e064c9fcdfc
Cameron Little <cameron@remitly.com>
2020-05-15 12:11:07 +0200

    Improve go version changing

--- go/go.bash
+++ go/go.bash
@@ -1,14 +1,19 @@
 if command -v go >/dev/null 2>&1; then
+    export GOROOT=$(cd $(dirname $(command -v go)); cd $(dirname $(dirname $(readlink $(command -v go)))); pwd)
     export GOPATH="$HOME/.go"
     export PATH="$PATH:$GOPATH/bin"
 
-    function chgo() {
-        local version
-        if [ -n "$1" ]
-        then
-            version="@$1"
-        fi
-        brew unlink $(brew list | grep '^go')
-        brew link --overwrite --force "go$version"
-    }
+    if test -L $(command -v go); then
+        function chgo() {
+            local version
+            if [ -n "$1" ]
+            then
+                version="@$1"
+            fi
+            brew unlink $(brew list | grep '^go')
+            brew link --overwrite --force "go$version"
+
+            export GOROOT=$(cd $(dirname $(command -v go)); cd $(dirname $(dirname $(readlink $(command -v go)))); pwd)
+        }
+    fi
 fi
