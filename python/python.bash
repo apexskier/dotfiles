adce3507361ad1c25a1b8d7ff36d629b47843e20
Cameron Little <cameron@camlittle.com>
2020-06-21 14:38:06 +0200

    Python fixes
    - Fix extra whitespace in python virtualenv in prompt
    - Use python3 for virtualenvwrapper

--- python/python.bash
+++ python/python.bash
@@ -2,14 +2,17 @@ if ! command -v python >/dev/null 2>&1; then
     exit 0
 fi
 
-mkdir -p $HOME/.virtualenvs
-export WORKON_HOME="$HOME/.virtualenvs"
-export PIP_VIRTUALENV_BASE=$WORKON_HOME
-export PIP_RESPECT_VIRTUALENV=true
-export VIRTUALENVWRAPPER_VIRTUALENV="virtualenv"
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/caches
+export PIP_RESPECT_VIRTUALENV=true
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+export VIRTUALENVWRAPPER_VIRTUALENV="virtualenv"
+export WORKON_HOME="$HOME/.virtualenvs"
+
+mkdir -p "$WORKON_HOME"
 
 [ -f /usr/local/bin/virtualenvwrapper.sh ] && . /usr/local/bin/virtualenvwrapper.sh
+
 if [ -d ~/Library/Python/3.7/bin ]; then
     export PATH="$PATH:~/Library/Python/3.7/bin"
 fi
