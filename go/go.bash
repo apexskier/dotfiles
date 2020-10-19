36e3187c6f9a49d1b47111718b1d170c9d70683d
Cameron Little <cameron@remitly.com>
2020-10-19 18:47:46 +0200

    Update chgo to have different GOPATHs for each version

--- go/go.bash
+++ go/go.bash
@@ -2,20 +2,27 @@ if ! command -v go >/dev/null 2>&1; then
     return 0
 fi
 
-export GOROOT=$(cd $(dirname $(command -v go)); cd $(dirname $(dirname $(readlink $(command -v go)))); pwd)/libexec
-export GOPATH="$HOME/.go"
-export PATH="$PATH:$GOPATH/bin"
+function _chgo_env() {
+    export GOROOT=$(cd $(dirname $(command -v go)); cd $(dirname $(dirname $(readlink $(command -v go)))); pwd)/libexec
+    export GO_VERSION=$(go version | cut -d' ' -f3)
 
-if test -L $(command -v go); then
-    function chgo() {
-        local version
-        if [ -n "$1" ]
-        then
-            version="@$1"
-        fi
-        brew unlink $(brew list | grep '^go')
-        brew link --overwrite --force "go$version"
+    # I should remove the old gopath from PATH, but that's a pain in the ass
+    # and this isn't getting run much
 
-        export GOROOT=$(cd $(dirname $(command -v go)); cd $(dirname $(dirname $(readlink $(command -v go)))); pwd)/libexec
-    }
-fi
+    export GOPATH="$HOME/.$GO_VERSION"
+    export PATH="$PATH:$GOPATH/bin"
+}
+
+function chgo() {
+    local version
+    if [ -n "$1" ]
+    then
+        version="@$1"
+    fi
+    brew unlink $(brew list | grep -E '^go(@|$)')
+    brew link --overwrite --force "go$version"
+
+    _chgo_env
+}
+
+_chgo_env
