9bdc592612c7507e7e28dd2c172be7bb9141aa88
Cameron Little <cameron@remitly.com>
2021-08-25 16:23:15 -0600

    various things

--- git/git.bash
+++ git/git.bash
@@ -7,9 +7,10 @@ then
     return 0
 fi
 
-function _git_nuke() { _git_branch; }
-function _git_sm() { _git_submodule; }
+function _git_nuke { _git_branch; }
+function _git_sm { _git_submodule; }
 function _git_squash_merge_rebase { _git_branch; }
+function _git_ff { _git_branch; }
 
 __git_complete gi __git_main
 __git_complete gti __git_main
