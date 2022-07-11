5483ded25281ac1a2ead459a49ab2a34f59c2bdc
Cameron Little <cameron@remitly.com>
2022-07-11 09:43:03 -0500

    more brew completion again

--- homebrew/homebrew.1.bash
+++ homebrew/homebrew.1.bash
@@ -11,5 +11,6 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 bp=$(brew --prefix)
 [ -f "$bp/git/contrib/completion/git-completion.bash" ] && . "$bp/git/contrib/completion/git-completion.bash"
 [ -f "$bp/etc/bash_completion" ] && . "$bp/etc/bash_completion"
+[ -f "$bp/etc/profile.d/bash_completion.sh" ] && . "$bp/etc/profile.d/bash_completion.sh"
 
 export PATH="$PATH:$bp/opt/curl/bin"
