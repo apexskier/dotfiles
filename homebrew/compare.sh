62a7683a1baa3fdeeb4bcaf3cf4f8242cab4dae2
Cameron Little <cameron@camlittle.com>
2025-07-08 18:38:09 -0600

    brewfile cleanup

--- homebrew/compare.sh
+++ homebrew/compare.sh
@@ -5,4 +5,6 @@
 # I have a bunch of stuff installed on my work computer that I don't actually
 # want in my dotfiles, so I don't want to just dump directly
 
+# TODO: try to figure out how to use `brew leaves`
+
 vimdiff <(brew bundle --describe dump --file=-) ~/.dotfiles/homebrew/Brewfile
