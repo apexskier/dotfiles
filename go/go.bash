3bb84dea36e63b9c2c21aa76cbfce7da5f9444f3
Cameron Little <cameron@camlittle.com>
2025-10-08 15:11:45 -0600

    Simplify go setup to use GOTOOLCHAIN
    (experimental)

--- go/go.bash
+++ go/go.bash
@@ -1,40 +1,17 @@
-function _chgo_env() {
-    GO_BIN_PATH=$(command -v go 2>/dev/null)
-    GO_BIN_AVAILABLE=$?
-    if [ $GO_BIN_AVAILABLE -ne 0 ]; then
-        return 1
-    fi
-
-    GOROOT=$(cd "$(dirname "$GO_BIN_PATH")" || return; cd "$(dirname "$(dirname "$(readlink "$GO_BIN_PATH")")")" || return; pwd)/libexec
-    GO_VERSION=$(go version | cut -d' ' -f3)
-    export GOROOT
-    export GO_VERSION
-
-    # I should remove the old gopath from PATH, but that's a pain in the ass
-    # and this isn't getting run much
-
-    export GOPATH="$HOME/.$GO_VERSION"
-    export PATH="$PATH:$GOPATH/bin"
-}
-
 function chgo() {
     local version
     local gomodfile
 
     if [ -n "$1" ]
     then
-        version="@$1"
+        version="$1"
     elif gomodfile=$(file_in_tree go.mod)
     then
         version="$(grep '^go ' "$gomodfile")"
-        version="@${version:3}"
+        version="${version:3}"
     fi
 
-    INSTALLED_GOS=$(brew list | grep -E '^go(@|$)')
-    brew unlink ${INSTALLED_GOS[@]}
-    brew link --overwrite --force "go$version"
-
-    _chgo_env
+    export GOTOOLCHAIN="go$version"
 }
 
-_chgo_env
+chgo "" # on shell start, set to go.mod version if available
