643b125120d5c152714fc0bded8db9c790ae22d2
Cameron Little <cameron@camlittle.com>
2025-11-11 11:46:22 -0700

    go toolchain fixes

--- go/go.bash
+++ go/go.bash
@@ -1,17 +1,20 @@
 function chgo() {
-    local version
+    local gotoolchain
     local gomodfile
 
-    if [ -n "$1" ]
+    if [ "$1" == "local" ]
     then
-        version="$1"
+        gotoolchain="local"
+    elif [ -n "$1" ]
+    then
+        gotoolchain="go$1"
     elif gomodfile=$(file_in_tree go.mod)
     then
-        version="$(grep '^go ' "$gomodfile")"
-        version="${version:3}"
+        gotoolchain="$(grep '^go ' "$gomodfile")"
+        gotoolchain="go${gotoolchain:3}"
     fi
 
-    export GOTOOLCHAIN="go$version"
+    export GOTOOLCHAIN="$gotoolchain"
 }
 
-chgo "" # on shell start, set to go.mod version if available
+chgo "local" # on shell start, set to go.mod version if available
