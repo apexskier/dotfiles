61bc2c9ba0ebb627a102fd3efc75ce1e774b917b
Cameron Little <cameron@camlittle.com>
2025-11-11 11:48:06 -0700

    gcloud

---
+++ gcloud/gcloud.bash
@@ -0,0 +1,9 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/Developer/google-cloud-sdk/path.bash.inc" ]; then
+    . "$HOME/Developer/google-cloud-sdk/path.bash.inc"
+fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/Developer/google-cloud-sdk/completion.bash.inc" ]; then
+    . "$HOME/Developer/google-cloud-sdk/completion.bash.inc"
+fi
